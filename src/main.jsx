bd512146509b4d299f5710eceeae9d1d9fe1fa48
ListoQasa <listoqasa@proton.me>
2025-10-31 18:00:13 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Buy from "./Buy";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/buy" element={<Buy />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
