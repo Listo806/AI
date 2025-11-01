1047c18b7110a1c909a2fe4eea1b99446f32eeb9
ListoQasa <listoqasa@proton.me>
2025-10-31 19:19:48 -0500

    Add Checkout and ThankYou routes to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Main pages
 import App from "./App";
 import Buy from "./Buy";
 import Sell from "./Sell";
@@ -9,17 +11,26 @@ import Assistant from "./Assistant";
 import ListProperty from "./ListProperty";
 import Owners from "./Owners";
 
+// Payment + post-checkout flow
+import Checkout from "./Checkout";
+import ThankYou from "./ThankYou";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
+        {/* Main public site */}
         <Route path="/" element={<App />} />
         <Route path="/buy" element={<Buy />} />
         <Route path="/sell" element={<Sell />} />
         <Route path="/rent" element={<Rent />} />
         <Route path="/assistant" element={<Assistant />} />
         <Route path="/list-property" element={<ListProperty />} />
+
+        {/* Owner plan funnel */}
         <Route path="/owners" element={<Owners />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/thank-you" element={<ThankYou />} />
       </Routes>
     </Router>
   </React.StrictMode>
