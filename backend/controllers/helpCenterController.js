eee27b7c7457d80b309e79251c45fd67c7021824
ListoQasa <listoqasa@proton.me>
2025-12-25 22:08:20 -0500

    Add HelpArticle controller for article management

--- backend/controllers/helpCenterController.js
+++ backend/controllers/helpCenterController.js
@@ -1,1 +1,20 @@
 
+const HelpArticle = require("../models/HelpArticle");
+
+exports.createArticle = async (req, res) => {
+  try {
+    const article = await HelpArticle.create(req.body);
+    res.status(201).json(article);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getArticles = async (req, res) => {
+  try {
+    const articles = await HelpArticle.findAll();
+    res.json(articles);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
