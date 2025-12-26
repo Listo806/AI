cfbf57dd69b939da45ad0ee8450ce19420048bb5
ListoQasa <listoqasa@proton.me>
2025-12-25 22:41:33 -0500

    Implement geocode function in mapboxService
    Add geocode function to fetch location data from Mapbox API

--- backend/services/mapboxService.js
+++ backend/services/mapboxService.js
@@ -1,1 +1,14 @@
+const axios = require("axios");
+
+module.exports = {
+  async geocode(address) {
+    const { data } = await axios.get(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+        address
+      )}.json`,
+      { params: { access_token: process.env.MAPBOX_KEY } }
+    );
+    return data.features[0];
+  },
+};
 
