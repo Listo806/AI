d60400d8b5920a976e2cb593ec636a7957aed729
hongocthem <hongocthem@github.com>
2026-06-30 13:09:55 +0700

    update

--- ninja_backend/src/pipeline/pipeline.module.ts
+++ ninja_backend/src/pipeline/pipeline.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { PipelineController } from "./pipeline.controller";
 import { PipelineService } from "./pipeline.service";
 import { DatabaseModule } from "../database/database.module";
+import { SubscriptionsModule } from "../subscriptions/subscriptions.module";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, SubscriptionsModule],
   controllers: [PipelineController],
   providers: [PipelineService],
   exports: [PipelineService],
