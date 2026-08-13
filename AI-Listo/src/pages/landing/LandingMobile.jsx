38e6b231de1134100533c52f8cd6300ed87a9a3a
hongocthem <hongocthem@github.com>
2026-08-13 22:23:39 +0700

    update CTA mobile

--- AI-Listo/src/pages/landing/LandingMobile.jsx
+++ AI-Listo/src/pages/landing/LandingMobile.jsx
@@ -2737,7 +2737,7 @@ export default function LandingMobile() {
                 <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />
               </svg>
               <div className="trial-wrap">
-                <span>{tr.finalCtaBtn}</span>
+                <span>{tr.ctaButtonText}</span>
               </div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
