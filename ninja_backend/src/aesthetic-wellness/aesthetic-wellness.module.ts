7a4202467c0466933331931e10484afa7c39b5a2
hongocthem <hongocthem@github.com>
2026-09-15 17:29:15 +0700

    update

--- ninja_backend/src/aesthetic-wellness/aesthetic-wellness.module.ts
+++ ninja_backend/src/aesthetic-wellness/aesthetic-wellness.module.ts
@@ -1,16 +1,18 @@
-import {Module} from "@nestjs/common";
-import {DatabaseModule} from "../database/database.module";
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
+import { AestheticWellnessController } from './aesthetic-wellness.controller';
+import { AestheticWellnessService } from './aesthetic-wellness.service';
+import { WorkspaceAiSetupService } from './workspace-ai-setup.service';
 import {WorkspacesModule} from "../workspaces/workspaces.module";
 import {PaymentGuard} from "../auth/guards/payment.guard";
-import {AestheticWellnessController} from "./aesthetic-wellness.controller";
-import {AestheticWellnessService} from "./aesthetic-wellness.service";
 import {AestheticClientsController} from "./aesthetic-clients.controller";
 import {AestheticClientsService} from "./aesthetic-clients.service";
 
 @Module({
- imports:[DatabaseModule,WorkspacesModule],
- controllers:[AestheticWellnessController,AestheticClientsController],
- providers:[PaymentGuard,AestheticWellnessService,AestheticClientsService],
- exports:[AestheticWellnessService,AestheticClientsService],
+  imports: [DatabaseModule, SubscriptionsModule],
+  controllers:[AestheticWellnessController,AestheticClientsController],
+  providers:[PaymentGuard,AestheticWellnessService,AestheticClientsService],
+  exports:[AestheticWellnessService,AestheticClientsService],
 })
-export class AestheticWellnessModule{}
+export class AestheticWellnessModule {}
